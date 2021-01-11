0d0a3a81c1ef5c0822df6f272fbf9bdad9d8ef00
KimTaewan <wwan13@naver.com>
2021-01-11 21:39:17 +0900

    API

--- src/main/java/springintroduction/springintroduction/controller/HelloController.java
+++ src/main/java/springintroduction/springintroduction/controller/HelloController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -30,5 +31,35 @@ public class HelloController {
         // return 값은 이 동작을 수행해줄 .html 파일의 이름을 넣어줘야함
         String file_name = "hello-template";
         return file_name;
+    }
+
+    // http 통신 방식을 이용해 문자만 전달
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello" + name;
+    }
+
+    // json 으로 데이터 전달 (API)
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+
+        // hello 객체를 json 변환 (jackson library) 를 통해 json 을 변환 한 후 클라이언트에 전달
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
