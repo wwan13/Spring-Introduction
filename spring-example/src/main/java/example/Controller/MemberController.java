d054b5f5a9f1962c0f69e91856bee591b74c6e7a
KimTaewan <wwan13@naver.com>
2021-01-12 21:05:52 +0900

    add form logic

--- spring-example/src/main/java/example/Controller/MemberController.java
+++ spring-example/src/main/java/example/Controller/MemberController.java
@@ -1,9 +1,11 @@
 package example.Controller;
 
+import example.domain.Member;
 import example.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /*
 * @Controller, @Service, @Repository -> 컴포넌트 스캔 방식으로 자동으로 의존성 주입 ( 스프링 빈에 주입 )
@@ -23,5 +25,17 @@ public class MemberController {
     @GetMapping("/members/new")
     public String createForm() {
         return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        System.out.println(member.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 }
