81159a642b8e21f5bfa8b97280b9ef6be7c47a6b
KimTaewan <wwan13@naver.com>
2021-01-13 17:50:22 +0900

    jdbc

--- spring-example/src/main/java/example/repository/MemoryMemberRepository.java
+++ spring-example/src/main/java/example/repository/MemoryMemberRepository.java
@@ -5,14 +5,15 @@ import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
-
 /*
 * Repository -> 데이터베이스 관련 로직들. 다양한 query 등의 메소드를 작성하는 클래스
 * @Repository 를 작성 해 주면 스프링 컨테이너에 자동으로 등록
 * */
+
+/**
+ * 메모리를 이용한
+ */
 //@Repository
-
-
 public class MemoryMemberRepository implements MemberRepository {
 
     private static Map<Long, Member> store = new HashMap<>();
