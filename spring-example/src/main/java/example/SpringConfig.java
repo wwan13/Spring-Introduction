3398a22c1aa1705a6e193f6391ed3a6aba020089
KimTaewan <wwan13@naver.com>
2021-01-14 11:14:09 +0900

    edit Spring Configuration

--- spring-example/src/main/java/example/SpringConfig.java
+++ spring-example/src/main/java/example/SpringConfig.java
@@ -1,11 +1,15 @@
 package example;
 
 import example.domain.Member;
+import example.repository.JdbcMemberRepository;
 import example.repository.MemberRepository;
 import example.repository.MemoryMemberRepository;
 import example.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 /*
 * 자바 코드로 스프링에 의존성을 부여하는 방법
@@ -14,13 +18,25 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SpringConfig {
 
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
     }
 
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
+
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
