a561f3ea46d223bdac2f2c348378b571055671f3
KimTaewan <wwan13@naver.com>
2021-01-14 22:07:50 +0900

    spring jpa

--- spring-example/src/main/java/example/SpringConfig.java
+++ spring-example/src/main/java/example/SpringConfig.java
@@ -17,30 +17,39 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig<å> {
 
-    private DataSource dataSource;
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+//    private DataSource dataSource;
 //
 //    @Autowired
 //    public SpringConfig(DataSource dataSource) {
 //        this.dataSource = dataSource;
 //    }
 
-    private EntityManager em;
+//    private EntityManager em;
 
-    @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
-    }
+//    @Autowired
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
+
+//    @Bean
+//    public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//        return new JpaMemberRepository(em);
+//
+//    }
 }
---
+++ spring-example/src/main/java/example/repository/SpringDataJpaRepository.java
@@ -0,0 +1,12 @@
+package example.repository;
+
+import example.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaRepository extends JpaRepository<Member, Long>, MemberRepository {
+
+    @Override
+    Optional<Member> findByName(String name);
+}
